39223a96abfa38dec506366a9f2860457e751cae
Norton Glitz <norton.glitz@gmail.com>
2023-10-08 02:11:17 -0300

    Small bug fixes

--- README.md
+++ README.md
@@ -1,36 +1,6 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+### Description
+This is an experimental app for study purpose.
+This project uses an API provided by [U.S. DEPARTMENT OF AGRICULTURE](https://www.usda.gov/) which gives data about foods.
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### Technologies
+[![An old rock in the desert](/assets/images/shiprock.jpg "Shiprock, New Mexico by Beau Rogers")](https://www.flickr.com/photos/beaurogers/31833779864/in/photolist-Qv3rFw-34mt9F-a9Cmfy-5Ha3Zi-9msKdv-o3hgjr-hWpUte-4WMsJ1-KUQ8N-deshUb-vssBD-6CQci6-8AFCiD-zsJWT-nNfsgB-dPDwZJ-bn9JGn-5HtSXY-6CUhAL-a4UTXB-ugPum-KUPSo-fBLNm-6CUmpy-4WMsc9-8a7D3T-83KJev-6CQ2bK-nNusHJ-a78rQH-nw3NvT-7aq2qf-8wwBso-3nNceh-ugSKP-4mh4kh-bbeeqH-a7biME-q3PtTf-brFpgb-cg38zw-bXMZc-nJPELD-f58Lmo-bXMYG-bz8AAi-bxNtNT-bXMYi-bXMY6-bXMYv)
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import InputFoodSearch from '@/components/InputFoodSearch'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Technology = ({ name, dir }: { name: string, dir: string }) =>
-  <div className="flex flex-col items-center">
-    <Image src={dir} className="m-2 mx-10 h-auto" alt="NextJS" height={50} width={65} />
-    <p>{name}</p>
-  </div>
+const Technology = ({ name, dir }: { name: string, dir: string, href: string }) =>
+  <Link href="https://nextjs.org/" target="_blank" className="p-2">
+    <div className="flex flex-col items-center">
+      <Image src={dir} className="m-2 mx-10 h-[65px] w-[65px]" alt={name} width={65} height={65} />
+      <p>{name}</p>
+    </div>
+  </Link>
 
 
 export default function HomePage() {
@@ -17,28 +19,15 @@ export default function HomePage() {
         <h1 className="xs:text-xs md:text-lg text-center mb-2">Look for the food that most suit your needs</h1>
         <InputFoodSearch />
       </div>
-      <div className="divider mt-[32vh]" />
-      <p>The source code is available in my profile on <Link className="link" href="https://github.com/nortonglitz/food-info">GitHub</Link>.</p>
-      <p className="italic">This app was created as a showcase, using an external API provided by <Link className="link" href="https://www.usda.gov/">U.S. DEPARTMENT OF AGRICULTURE</Link>, and technlogies listed below.</p>
-      <div className="flex justify-around items-baseline flex-nowrap overflow-x-auto w-full mt-2">
-        <Link href="https://nextjs.org/" target="_blank">
-          <Technology name="NextJS" dir="/next.svg" />
-        </Link>
-        <Link href="https://git-scm.com/" target="_blank">
-          <Technology name="Git" dir="/git.svg" />
-        </Link>
-        <Link href="https://github.com/" target="_blank">
-          <Technology name="Github" dir="/github.svg" />
-        </Link>
-        <Link href="https://react.dev/" target="_blank">
-          <Technology name="React" dir="/react.svg" />
-        </Link>
-        <Link href="https://tailwindcss.com/" target="_blank">
-          <Technology name="TailwindCSS" dir="/tailwindcss.svg" />
-        </Link>
-        <Link href="https://www.typescriptlang.org/" target="_blank">
-          <Technology name="TypeScript" dir="/typescript.svg" />
-        </Link>
+      <div className="divider mt-[35vh]" />
+      <p className="italic text-sm text-center">This app was created as a showcase, using an external API provided by <Link className="link" target="_blank" href="https://www.usda.gov/">U.S. DEPARTMENT OF AGRICULTURE</Link>, and technologies listed below.</p>
+      <div className="flex justify-around items-baseline flex-wrap overflow-x-auto w-full gap-2">
+        <Technology name="NextJS" dir="/next.svg" href="https://nextjs.org/" />
+        <Technology name="Git" dir="/git.svg" href="https://git-scm.com/" />
+        <Technology name="Github" dir="/github.svg" href="https://github.com/" />
+        <Technology name="React" dir="/react.svg" href="https://react.dev/" />
+        <Technology name="TailwindCSS" dir="/tailwindcss.svg" href="https://tailwindcss.com/" />
+        <Technology name="TypeScript" dir="/typescript.svg" href="https://www.typescriptlang.org/" />
       </div>
     </main>
   )
