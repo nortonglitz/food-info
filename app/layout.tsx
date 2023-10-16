0d1f98ccd0b44b31fcbab9d94d257474fd3d8b41
Norton Glitz <norton.glitz@gmail.com>
2023-10-16 14:58:59 -0300

    Notification work on all devices properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={robotoSlab.className}>
-        <dialog open className="open:relative open:w-full open:flex open:justify-between open:px-4 open:py-2">
-          <div className="flex items-center text-gray-400 font-mono text-sm">
-            <Image src="/github.svg" className="h-[30px] w-[30px] mr-2 bg-slate-400 rounded-full ring-2 ring-gray-400" alt="Github" width={65} height={65} />
-            You can find the source code in my&nbsp;<Link className="link" href="https://github.com/nortonglitz/food-info" target="_blank">Github repository</Link>.
+        <dialog open className="open:mt-2 open:px-2 open:flex open:justify-center open:w-full open:bg-transparent bottom-10 md:top-20">
+          <div className="flex justify-between px-4 py-3 items-center rounded bg-zinc-900/[0.7]">
+            <div className="flex items-center text-gray-400 font-mono text-sm">
+              <Image src="/github.svg" className="h-[30px] w-[30px] mr-2 bg-slate-400 rounded-full ring-2 ring-gray-400" alt="Github" width={65} height={65} />
+              <div>
+                You can find the source code in my Github&nbsp;<Link className="link" href="https://github.com/nortonglitz/food-info" target="_blank">repository</Link>.
+              </div>
+            </div>
+            <form method="dialog" className="ml-4 mr-2">
+              <button className="link-neutral text-gray-400 hover:text-gray-300 font-semibold transition-colors">
+                X
+              </button>
+            </form>
           </div>
-          <form method="dialog">
-            <button className="link-neutral text-gray-400 hover:text-gray-300 mr-10 font-semibold transition-colors">
-              X
-            </button>
-          </form>
         </dialog>
         <nav className="navbar bg-neutral text-neutral-content">
           <Link href="/" className="flex flex-col items-start ml-2">
